f15cc3a6ab0046b1ca3ae17a582807e4356c2373
Fidel Leal <127764786+fidel-techprep@users.noreply.github.com>
2023-07-30 11:28:46 +0000

    add more comments and clean up

--- app.rb
+++ app.rb
@@ -15,20 +15,27 @@ end
 json_data = File.read('data.json')
 dictionary = JSON.parse(json_data, symbolize_names: true)
 
+# Authentication method
 def authenticate
+  # Extract token from header
   token = request.env['HTTP_AUTHORIZATION']&.split(' ')&.last
+
+  # Halt response and resturn error if token is invalid
   halt 401, error_response(401, 'Unauthorized') unless token == API_KEY || token == PERSONAL_API_KEY 
 end
 
+# Authentication for API routes
 before('/api/*') do
   authenticate
 end
 
+# Return a random word
 get("/api/random-word") do
   content_type :json
   { word: dictionary.sample }.to_json
 end
 
+# Return the score for a requested word
 get("/api/word/:search_word") do
   content_type :json
   search_word = params['search_word'].to_sym
