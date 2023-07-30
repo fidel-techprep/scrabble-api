e44d969353a56365c76ceb1c65c18763e287fe16
Fidel Leal <127764786+fidel-techprep@users.noreply.github.com>
2023-07-30 02:08:02 +0000

    add error handling and token authentication

--- app.rb
+++ app.rb
@@ -1,19 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
 
+API_KEY = ENV.fetch("API_KEY")
+PERSONAL_API_KEY = ENV.fetch("PERSONAL_API_KEY")
+
+# Helper method to return JSON error responses
+def error_response(status, message)
+  content_type :json
+  status status
+  { error: message }.to_json
+end
 
 # Load words from the JSON file into an array
-
 json_data = File.read('data.json')
 dictionary = JSON.parse(json_data, symbolize_names: true)
 
-def authenticate!
+def authenticate
   token = request.env['HTTP_AUTHORIZATION']&.split(' ')&.last
-  #halt 401, error_response(401, 'Unauthorized') unless token == API_TOKEN
+  halt 401, error_response(401, 'Unauthorized') unless token == API_KEY || token == PERSONAL_API_KEY 
 end
 
 before('/api/*') do
-  authenticate!
+  authenticate
 end
 
 get("/api/random-word") do
@@ -24,5 +32,15 @@ end
 get("/api/word/:search_word") do
   content_type :json
   search_word = params['search_word'].to_sym
-  matching_word = dictionary.fetch(search_word).to_json ||  { error: "Not found" }.to_json
+  matching_word = dictionary.fetch(search_word, { error: "Not found" }).to_json
+end
+
+# Error handling for invalid endpoints
+not_found do
+  error_response 404, 'Endpoint not found'
+end
+
+# Error handling for other server errors
+error do
+  error_response 500, 'Internal server error'
 end
